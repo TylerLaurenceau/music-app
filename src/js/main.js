3aaca6172cf6e35974f61e4eb19c7ceb74e5862c
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-02-17 15:09:41 -0500

    clicking pictures plays song but not correct one yet

--- src/js/main.js
+++ src/js/main.js
@@ -15,21 +15,34 @@ function test(data, callback) {
 
 //______________________________________________________________________________
 function processSongs(songs) {
+    console.log(songs)
     for (var i = 0; i < songs.length; i++) {
         $(".content").append(`<div class = "art"><img src = "${songs[i].artwork_url}"/><div>
                                   <div class = "title">${songs[i].title}</div>`)
     }
-    console.log(songs);
+    $(".art img").click(pictureClicker);
 }
 //______________________________________________________________________________
-$('#searchButton').click(clicker);
+$("#searchButton").click(searchClicker);
 
-function clicker(event) {
+function searchClicker(event) {
     event.preventDefault();
     var searchBar = $("#searchBar").val();
     test(searchBar, processSongs);
 };
 //______________________________________________________________________________
+function pictureClicker(event) {
+    event.preventDefault();
+    var searchBar = $("#searchBar").val();
+    test(searchBar, playSong);
+    console.log("music")
+};
+//______________________________________________________________________________
+function playSong(songs) {
+    for (var i = 0; i < songs.length; i++) {
+        $(".music").html(`<audio src="${songs[i].stream_url}?client_id=${token}" controls></audio>`)
+    }
+};
 
 
 //$(".music").html(`<audio src="https://api.soundcloud.com/tracks/259021543/stream?client_id=${token}" controls></audio>`)
